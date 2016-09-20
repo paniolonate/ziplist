beee8e35301ed56f6f843fcee11f4ee355e140d4
paniolonate <nahinan@hawaii.edu>
2016-09-19 23:20:04 -1000

    finished ziplist

---
+++ ziplist.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Nathan Nahina on 9/19/2016.
+ */
+
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
+
+function zipList(list1, list2){
+  const resultList = [];
+  for (let i = 0; i < list1; i++){
+    resultList.push(list1[i], list2[i]);
+  }
+  return resultList;
+}
+
+//console.log(zipList(testList1, testList2));
+
+function zipListTheSimpleWay(list1, list2){
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(testList1, testList2));
